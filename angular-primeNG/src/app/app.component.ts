d11f366edb88e385e66961ada33959ea2c2d5644
wangxiangyu <15724580513@163.com>
2019-09-07 22:55:22 +0800

    update

---
+++ angular-primeNG/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+
+  title = 'NiceFish';
+  public hideSidebar = false;
+
+  SidebarStatusChange(): void {
+
+
+    this.hideSidebar = !this.hideSidebar;
+  }
+
+}

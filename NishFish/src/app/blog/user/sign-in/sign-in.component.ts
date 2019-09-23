3138d966b69ea493d8b3989f7ac9a26cd30f9e4c
wangxiangyu <15724580513@163.com>
2019-09-23 20:12:43 +0800

    update

--- NishFish/src/app/blog/user/sign-in/sign-in.component.ts
+++ NishFish/src/app/blog/user/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ActivatedRouteSnapshot, RouterState, RouterStateSnapshot } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  public userInfo: any = {
+    username: '',
+    password: '',
+    email: '',
+  };
+
+
+  constructor(
+    public router: Router,
+    public activatedRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    console.log('--- sign-in-component ---');
+    console.log('router => ', this.router);
+    console.log('activatedRoute => ', this.activatedRoute);
+
+    const activatedRouteSnapshot: ActivatedRouteSnapshot = this.activatedRoute.snapshot;
+    const routerState: RouterState = this.router.routerState;
+    const routerStateSnapshot: RouterStateSnapshot = routerState.snapshot;
+
+    console.log('activatedRouteSnapshot => ', activatedRouteSnapshot);
+    console.log('routerState => ', routerState);
+    console.log('routerStateSnapshot => ', routerStateSnapshot);
   }
 
+
+  doLogin(data): void {
+    console.log(data.form);
+    if (data.form.status === 'VALID') {
+      console.log(this.userInfo);
+      console.log(data);
+      this.router.navigateByUrl('/home');
+    }
+
+  }
+
+  retrievePwd(): void {
+    this.router.navigateByUrl('logout');
+  }
 }
--- NishFish/src/app/shared/shared.module.ts
+++ NishFish/src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule } from '@angular/forms';
+
 import { MenubarModule } from 'primeng/menubar';
 import { TieredMenuModule } from 'primeng/tieredmenu';
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
 import { MenuModule } from 'primeng/menu';
 import { PanelMenuModule } from 'primeng/panelmenu';
+import { PanelModule } from 'primeng/panel';
 
 import { SignInComponent } from '../blog/user/sign-in/sign-in.component';
 import { SignUpComponent } from '../blog/user/sign-up/sign-up.component';
@@ -18,20 +21,25 @@ import { SignUpComponent } from '../blog/user/sign-up/sign-up.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     MenubarModule,
     TieredMenuModule,
     ButtonModule,
     SidebarModule,
     MenuModule,
     PanelMenuModule,
+    PanelModule,
   ],
   exports: [
+    CommonModule,
+    FormsModule,
     MenubarModule,
     TieredMenuModule,
     ButtonModule,
     SidebarModule,
     MenuModule,
     PanelMenuModule,
+    PanelModule,
   ]
 })
 export class SharedModule { }

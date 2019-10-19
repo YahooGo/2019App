02a6daafd0b7a5bfe81e9093c3dbe6e084c73e4b
wangxiangyu <15724580513@163.com>
2019-10-19 21:04:15 +0800

    update

--- Angular820191006/README.md
+++ Angular820191006/README.md
@@ -3,7 +3,7 @@
 ## 版本
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.2.0.
 
-## 系统学习Angular第一天
+## 系统学习Angular
 
 > 创建组件： `ng g c components/MyComponentName`
 在组件内新建文件index.ts 导出组件所有内容以减少路径引用每级文件都有
@@ -44,12 +44,52 @@ ngStyle 由于是嵌入式样式，会覆盖掉其他样式，慎用
 
 ------------------------------------------
 
+> 组件的生命周期
 
+```
+// 声明周期是可选的 构造函数必有 
+constructor 构造函数永远首先被调用
+ngOnChanges 输入属性变化时被调用
+ngOnInit    组件初始化完成时被调用
+ngDoCheck   脏值检测时调用
 
+ngAfterContentInit    当内容投影ng-content完成时调用
+ngAfterContentChecked Angular检测投影内容时调用(多次)
+ngAfterViewInit       当组件视图(子视图)初始化完成时
+ngAfterVIewChecked    当检查视图变化时(多次)
 
+ngOnDestory           当组件销毁时
+```
 
+> @Input()输入值尽量使用get/set拦截，可以少写生命周期
+```
+  // 初始化时先声明内部属性 tabOpt
+  tabOpt: Options = {};
+  // 输入属性
+  @Input()
+  set Options(data: Options) {
+    this.tabOpt = Object.assign(this.tabOpt, data);
+  }
+  get Options() {
+    return this.tabOpt;
+  }
+```
 
+> 模板在组件类中的引用
+```
+<div #helloDiv></div>
+// `#`后面是给模板或DOM元素起一个引用名字，变可以在组件类或模板中进行，注意唯一
 
+exportt class AppComponent {
+  @ViewChild('helloDiv') helloDivRef: ElementRef;
+  // @ViewChild是一个选择器，用来查找要引用的DOM元素或者组件
+  // ElementRef 是DOM元素的一个包装类
+  // 因为DOM元素不是Angular中的类，所以用一个包装类在Angular中使用和表示其他类型
+  // helloDiv是模板引用变量名，helloDivRef是类中别名
+
+}
+
+```
 
 
 
--- Angular820191006/src/app/app.component.ts
+++ Angular820191006/src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { TopMenu, Options } from './Components';
-
+import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import { TopMenu, Options, ImageSlider, ImageSliderComponent } from './Components';
+import { interval } from 'rxjs';
+import { map, scan, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'App';
+
+  // @ViewChild('ImageSlider', { static: false }) imageSlider: ImageSliderComponent;
+  @ViewChild(ImageSliderComponent, { static: false }) imageSlider: ImageSliderComponent;
 
   TopMenu: TopMenu[] = [
     {
@@ -44,10 +48,6 @@ export class AppComponent {
       link: ''
     },
     {
-      title: '热门',
-      link: ''
-    },
-    {
       title: '男装',
       link: ''
     },
@@ -59,30 +59,42 @@ export class AppComponent {
       title: '男装',
       link: ''
     },
+  ];
+
+  /**
+   * 轮播图图片数组
+   */
+  imageSliders: ImageSlider[] = [
     {
-      title: '热门',
-      link: ''
+      imageUrl: 'http://img4.imgtn.bdimg.com/it/u=2551121849,3276036345&fm=26&gp=0.jpg',
+      link: '',
+      caption: '五更琉璃',
     },
     {
-      title: '男装',
-      link: ''
+      imageUrl: 'http://img4.imgtn.bdimg.com/it/u=3099443564,3000777564&fm=26&gp=0.jpg',
+      link: '',
+      caption: '五更琉璃',
     },
     {
-      title: '热门',
-      link: ''
+      imageUrl: 'http://img1.imgtn.bdimg.com/it/u=281639794,1295715339&fm=26&gp=0.jpg',
+      link: '',
+      caption: '五更琉璃',
     },
     {
-      title: '男装',
-      link: ''
+      imageUrl: 'http://img4.imgtn.bdimg.com/it/u=2551121849,3276036345&fm=26&gp=0.jpg',
+      link: '',
+      caption: '五更琉璃',
     },
     {
-      title: '热门',
-      link: ''
+      imageUrl: 'http://img4.imgtn.bdimg.com/it/u=3099443564,3000777564&fm=26&gp=0.jpg',
+      link: '',
+      caption: '五更琉璃',
     },
     {
-      title: '男装',
-      link: ''
-    },
+      imageUrl: 'http://img1.imgtn.bdimg.com/it/u=281639794,1295715339&fm=26&gp=0.jpg',
+      link: '',
+      caption: '五更琉璃',
+    }
   ];
 
   /**
@@ -102,5 +114,35 @@ export class AppComponent {
    */
   handleTabSelected(tabMenu: TopMenu) {
     console.log(tabMenu);
+  }
+
+  handleChange() {
+    this.tabScrollOptions = {
+      backgroundColor: 'yellow',
+      titleActiveColor: 'green',
+
+    };
+  }
+
+  createO() {
+    const d$ = interval(1000);
+    const b$ = d$.pipe(
+      startWith(5),
+      scan(time => time - 1)
+    );
+    b$.subscribe(item => console.log(item));
+  }
+
+  ngOnInit(): void {
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
+    // this.createO();
+
+  }
+
+  ngAfterViewInit(): void {
+    // Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    // Add 'implements AfterViewInit' to the class.
+    console.log('组件轮播图', this.imageSlider);
   }
 }
